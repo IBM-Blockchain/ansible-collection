36878d1e17b58d38d2a051a15327d802c3046443
Simon Stone <sstone1@uk.ibm.com>
2020-03-05 13:04:50 +0000

    Add peer info module
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

---
+++ plugins/modules/peer_info.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: peer_info
+short_description: Get information about a Hyperledger Fabric peer
+description:
+    - Get information about a Hyperledger Fabric peer by using the IBM Blockchain Platform.
+    - This module works with the IBM Blockchain Platform managed service running in IBM Cloud, or the IBM Blockchain
+      Platform software running in a Red Hat OpenShift or Kubernetes cluster.
+author: Simon Stone (@sstone1)
+options:
+    api_endpoint:
+        description:
+            - The URL for the IBM Blockchain Platform console.
+        type: str
+    api_authtype:
+        description:
+            - C(ibmcloud) - Authenticate to the IBM Blockchain Platform console using IBM Cloud authentication. You must provide
+              a valid API key using I(ibp_api_key).
+            - C(basic) - Authenticate to the IBM Blockchain Platform console using basic authentication. You must provide both a
+              valid API key using I(ibp_api_key) and API secret using I(ibp_api_secret).
+        type: str
+    api_key:
+        description:
+            - The API key for the IBM Blockchain Platform console.
+        type: str
+    api_secret:
+        description:
+            - The API secret for the IBM Blockchain Platform console.
+            - Only required when I(ibp_api_authtype) is C(basic).
+        type: str
+    api_timeout:
+        description:
+            - The timeout, in seconds, to use when interacting with the IBM Blockchain Platform console.
+        type: integer
+        default: 60
+    display_name:
+        description:
+            - The display name for the peer.
+    wait_timeout:
+        description:
+            - The timeout, in seconds, to wait until the peer is available.
+        type: integer
+        default: 60
+notes: []
+requirements: []
+'''
+
+EXAMPLES = '''
+'''
