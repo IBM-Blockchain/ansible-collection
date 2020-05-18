04fccd2ec9c5fbcf8704407ab08c4c9965437f3c
m-g-k <mgk@uk.ibm.com>
2020-05-18 11:07:56 +0000

    Allow users to easily change names in the playbooks (#234)
    * Allow users to easily change names in the playbooks
    
    Signed-off-by: m-g-k <mgk@uk.ibm.com>
    
    * Fix lint issues
    
    Signed-off-by: m-g-k <mgk@uk.ibm.com>
    
    * Fix remaining lint issues
    
    Signed-off-by: m-g-k <mgk@uk.ibm.com>
    
    Co-authored-by: Simon Stone <sstone1@uk.ibm.com>

--- tutorial/join_network.sh
+++ tutorial/join_network.sh
@@ -19,23 +19,23 @@ done
 COMMAND=${@:$OPTIND:1}
 if [ "${COMMAND}" = "join" ]; then
     set -x
-    ansible-playbook 11-create-endorsing-organization-components.yml --extra-vars "@org2-vars.yml"
+    ansible-playbook 11-create-endorsing-organization-components.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
     if [ "${IMPORT_EXPORT_REQUIRED}" = "1" ]; then
-        ansible-playbook 12-export-organization.yml --extra-vars "@org2-vars.yml"
-        ansible-playbook 13-import-organization.yml --extra-vars "@org1-vars.yml"
+        ansible-playbook 12-export-organization.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
+        ansible-playbook 13-import-organization.yml --extra-vars "@org1-vars.yml" --extra-vars "@common-vars.yml"
     fi
-    ansible-playbook 14-add-organization-to-channel.yml --extra-vars "@org1-vars.yml"
+    ansible-playbook 14-add-organization-to-channel.yml --extra-vars "@org1-vars.yml" --extra-vars "@common-vars.yml"
     if [ "${IMPORT_EXPORT_REQUIRED}" = "1" ]; then
-        ansible-playbook 15-import-ordering-service.yml --extra-vars "@org2-vars.yml"
+        ansible-playbook 15-import-ordering-service.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
     fi
-    ansible-playbook 16-join-peer-to-channel.yml --extra-vars "@org2-vars.yml"
-    ansible-playbook 17-add-anchor-peer-to-channel.yml --extra-vars "@org2-vars.yml"
+    ansible-playbook 16-join-peer-to-channel.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
+    ansible-playbook 17-add-anchor-peer-to-channel.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
     set +x
 elif [ "${COMMAND}" = "destroy" ]; then
     set -x
-    ansible-playbook 97-delete-endorsing-organization-components.yml --extra-vars "@org1-vars.yml"
-    ansible-playbook 98-delete-endorsing-organization-components.yml --extra-vars "@org2-vars.yml"
-    ansible-playbook 99-delete-ordering-organization-components.yml --extra-vars "@ordering-org-vars.yml"
+    ansible-playbook 97-delete-endorsing-organization-components.yml --extra-vars "@org1-vars.yml" --extra-vars "@common-vars.yml"
+    ansible-playbook 98-delete-endorsing-organization-components.yml --extra-vars "@org2-vars.yml" --extra-vars "@common-vars.yml"
+    ansible-playbook 99-delete-ordering-organization-components.yml --extra-vars "@ordering-org-vars.yml" --extra-vars "@common-vars.yml"
     set +x
 else
     usage
