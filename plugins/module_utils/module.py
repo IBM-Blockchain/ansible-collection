bd157b746f2e9a9b8cd032676f8c8b09bd77ea16
Simon Stone <sstone1@uk.ibm.com>
2020-06-02 12:26:24 +0100

    Document Fabric v1.4.3 requirement and enforce binary versions (resolves #265) (#266)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- plugins/module_utils/module.py
+++ plugins/module_utils/module.py
@@ -5,9 +5,11 @@
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils._text import to_native
+from distutils.version import LooseVersion
 
 import os
 import platform
+import re
 import subprocess
 
 HFC_IMPORT_ERR = None
@@ -30,6 +32,19 @@ except ImportError as e:
 def missing_required_bin(binary, reason=None, url=None):
     hostname = platform.node()
     msg = "Failed to execute the required binary (%s) on %s." % (binary, hostname)
+    if reason:
+        msg += " This is required %s." % reason
+    if url:
+        msg += " See %s for more info." % url
+
+    msg += (" Please read module documentation and install in the appropriate location."
+            " If the required binary is installed, then the install directory may not be present in the PATH environment variable (%s)." % os.environ['PATH'])
+    return msg
+
+
+def wrong_version_bin(binary, actual_version, expected_version, reason=None, url=None):
+    hostname = platform.node()
+    msg = "Required binary (%s) on %s has wrong version, expected version %s but version is %s." % (binary, hostname, expected_version, actual_version)
     if reason:
         msg += " This is required %s." % reason
     if url:
@@ -63,3 +78,10 @@ class BlockchainModule(AnsibleModule):
                 self.fail_json(msg=missing_required_bin(binary, url=url), exception=to_native(e), cmd=f'{binary} version')
             if process.returncode != 0:
                 self.fail_json(msg=missing_required_bin(binary, url=url), rc=process.returncode, stdout=process.stdout, stderr=process.stderr, cmd=f'{binary} version')
+            p = re.compile('Version: (.+)$', re.MULTILINE)
+            m = p.search(process.stdout)
+            if m is None:
+                self.fail_json(msg=wrong_version_bin(binary, '<unknown>', '>= 1.4.3', url=url), rc=process.returncode, stdout=process.stdout, stderr=process.stderr, cmd=f'{binary} version')
+            version = m.group(1)
+            if not LooseVersion(version) >= LooseVersion('1.4.3'):
+                self.fail_json(msg=wrong_version_bin(binary, version, '>= 1.4.3', url=url), rc=process.returncode, stdout=process.stdout, stderr=process.stderr, cmd=f'{binary} version')
