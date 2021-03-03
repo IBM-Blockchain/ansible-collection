85b062cda524248a60d871e76355867e994894e2
Simon Stone <sstone1@uk.ibm.com>
2021-03-03 21:00:05 +0000

    Add workflow to purge all lost components (#533)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- .github/scripts/purge.sh
+++ .github/scripts/purge.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 if [ "${API_AUTHTYPE}" = "basic" ]; then
-    curl -XDELETE -u "${API_KEY}:${API_SECRET}" "${API_ENDPOINT}/ak/api/v3/kubernetes/components/purge"
+    curl -f -k -XDELETE -u "${API_KEY}:${API_SECRET}" "${API_ENDPOINT}/ak/api/v3/kubernetes/components/purge"
 elif [ "${API_AUTHTYPE}" = "ibmcloud" ]; then
-    ACCESS_TOKEN=$(curl -XPOST d "apikey=${API_KEY}" -d grant_type=urn:ibm:params:oauth:grant-type:apikey https://iam.cloud.ibm.com/identity/token | jq -r .access_token)
-    curl -XDELETE -H "Authorization: Bearer ${ACCESS_TOKEN}" "${API_ENDPOINT}/ak/api/v3/kubernetes/components/purge"
+    ACCESS_TOKEN=$(curl -XPOST -d "apikey=${API_KEY}" -d grant_type=urn:ibm:params:oauth:grant-type:apikey https://iam.cloud.ibm.com/identity/token | jq -r .access_token)
+    curl -f -XDELETE -H "Authorization: Bearer ${ACCESS_TOKEN}" "${API_ENDPOINT}/ak/api/v3/kubernetes/components/purge"
 fi
